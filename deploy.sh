5ba11310ce732f1564e381bb64b490d626cc39b2
Narno <arnaud@ligny.org>
2016-06-27 00:48:08 +0200

    Try another method...

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ DEST_DIR="/lamp0/web/vhosts/phpoole.org/htdocs"
 
 echo "Starting to mirroring to SFTP"
 
-lftp -u $FTP_USER,$FTP_PASS $FTP_SITE -e "mirror -c -e -R $SRC_DIR $DEST_DIR ; exit"
+#lftp -u $FTP_USER,$FTP_PASS $FTP_SITE -e "mirror -c -e -R $SRC_DIR $DEST_DIR ; exit"
+sshpass -p "$FTP_PASS" scp -r $SRC_DIR $FTP_USER@$FTP_SITE:$DEST_DIR
 
 exit 0
