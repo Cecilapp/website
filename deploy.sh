45a314b622197ee5c81e855cec31fdcc10405b77
Arnaud Ligny <arnaud@ligny.org>
2016-06-27 01:56:30 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ DEST_DIR="/lamp0/web/vhosts/phpoole.org/htdocs"
 echo "Starting to mirroring to SFTP"
 
 #lftp -u $FTP_USER,$FTP_PASS $FTP_SITE -e "mirror -c -e -R $SRC_DIR $DEST_DIR ; exit"
-sshpass -p "$FTP_PASS" -o StrictHostKeyChecking=no scp -rp "$SRC_DIR" $FTP_USER@$FTP_SITE:$DEST_DIR
+sshpass -p "$FTP_PASS" scp -o StrictHostKeyChecking=no -rp "$SRC_DIR" $FTP_USER@$FTP_SITE:$DEST_DIR
 
 exit 0
