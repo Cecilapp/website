2d55be8a606b6eff53fa94d40a3acb898cf00c21
Travis <contact@travis-ci.org>
2017-11-11 02:45:45 +0000

    Travis build 1044 pushed to content/documentation

---
+++ content/documentation/api/cache/master/twig/93/933356b86090d5b854498a84741b9c0c311f81b9e9a4be5ee2bbaea4f44e679d.php
@@ -0,0 +1,99 @@
+<?php
+
+/* interfaces.twig */
+class __TwigTemplate_70b22f2bed7a9db242e632f09749d26b29885ef9d92d3dd176a8a6a7f91d19b0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("layout/layout.twig", "interfaces.twig", 1);
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'body_class' => array($this, 'block_body_class'),
+            'page_content' => array($this, 'block_page_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "layout/layout.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        $context["__internal_6d1d2c915426d4e668bbdb41d1833c756b253aa99a585bde5820bf8e4e21eeef"] = $this->loadTemplate("macros.twig", "interfaces.twig", 2);
+        // line 1
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        echo "Interfaces | ";
+        $this->displayParentBlock("title", $context, $blocks);
+    }
+
+    // line 4
+    public function block_body_class($context, array $blocks = array())
+    {
+        echo "interfaces";
+    }
+
+    // line 6
+    public function block_page_content($context, array $blocks = array())
+    {
+        // line 7
+        echo "    <div class=\"page-header\">
+        <h1>Interfaces</h1>
+    </div>
+
+    ";
+        // line 11
+        echo $context["__internal_6d1d2c915426d4e668bbdb41d1833c756b253aa99a585bde5820bf8e4e21eeef"]->getrender_classes((isset($context["interfaces"]) ? $context["interfaces"] : $this->getContext($context, "interfaces")));
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "interfaces.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 11,  49 => 7,  46 => 6,  40 => 4,  33 => 3,  29 => 1,  27 => 2,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends \"layout/layout.twig\" %}
+{% from \"macros.twig\" import render_classes %}
+{% block title %}Interfaces | {{ parent() }}{% endblock %}
+{% block body_class 'interfaces' %}
+
+{% block page_content %}
+    <div class=\"page-header\">
+        <h1>Interfaces</h1>
+    </div>
+
+    {{ render_classes(interfaces) }}
+{% endblock %}
+", "interfaces.twig", "/home/travis/build/PHPoole/PHPoole-library/vendor/sami/sami/Sami/Resources/themes/default/interfaces.twig");
+    }
+}
