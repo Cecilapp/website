d9d7162ae73fda69cc5c162a5d0294475a78e1ef
Arnaud Ligny <arnaud@ligny.org>
2020-03-24 23:07:26 +0100

    Update Netlify config

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -5,7 +5,7 @@ curl -sSOL https://github.com/Cecilapp/Cecil/releases/latest/download/cecil.phar
 php cecil.phar --version
 
 echo "Started Cecil build"
-if [ -z "$1" ]; then php cecil.phar build --quiet --optimize; else echo "URL: $1" && php cecil.phar build -v --baseurl=$1 --drafts || { sleep 30; false; }; fi
+if [ -z "$1" ]; then php cecil.phar build -v --optimize; else echo "URL: $1" && php cecil.phar build -v --baseurl=$1 --drafts || { sleep 30; false; }; fi
 
 # build success? can deploy?
 if [ $? = 0 ]; then echo "Finished Cecil build"; exit 0; fi
