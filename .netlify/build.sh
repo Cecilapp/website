e622375b85da5b7cd5cf92fd87522edf38bf9335
Arnaud Ligny <arnaud@ligny.org>
2020-12-02 01:34:20 +0100

    Update build.sh

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -2,6 +2,11 @@ echo "Downloading Cecil"
 curl -sSOL https://github.com/Cecilapp/Cecil/releases/latest/download/cecil.phar
 php cecil.phar --version
 
+echo "Started CSS build"
+npm install tailwindcss
+npm install @tailwindcss/typography
+npx tailwindcss-cli build ./static/tailwind.css -o ./static/styles.css
+
 echo "Started Cecil build"
 if [[ $1 == "preview" ]]; then
   php cecil.phar build -v --baseurl=$DEPLOY_PRIME_URL --drafts || { sleep 30; false; };
