a0ad67c30af097471b8a0cf48ff799cae5d2ceeb
Arnaud Ligny <arnaud@ligny.org>
2020-12-17 10:32:56 +0100

    Update build.sh

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,12 +1,16 @@
+CECIL_PHAR_URL=https://github.com/Cecilapp/Cecil/releases/latest/download/cecil.phar
+CSS_INPUT=./static/tailwind.css
+CSS_OUPUT=./static/styles.css
+
 echo "Downloading Cecil"
-curl -sSOL https://github.com/Cecilapp/Cecil/releases/latest/download/cecil.phar
+curl -sSOL $CECIL_PHAR_URL
 php cecil.phar --version
 
 echo "Started CSS build"
 npm install tailwindcss --silent
 npm install @tailwindcss/typography --silent
-npx tailwindcss-cli build ./static/tailwind.css -o ./static/styles.css
-if [ $? = 0 ]; then echo "Finished Tailwind build"; else echo "Tailwind fail..."; exit 1; fi
+npx tailwindcss-cli build $CSS_INPUT -o $CSS_OUPUT
+if [ $? = 0 ]; then echo "Finished CSS build"; else echo "CSS build fail..."; exit 1; fi
 
 echo "Started Cecil build"
 if [[ $1 == "preview" ]]; then
@@ -14,13 +18,14 @@ if [[ $1 == "preview" ]]; then
 else
   php cecil.phar build -v --baseurl=$URL --postprocess;
 fi
+if [ $? = 0 ]; then echo "Finished Cecil build"; else echo "Cecil build fail..."; exit 1; fi
 
 echo "Started Algolia index import"
 npm install -g @algolia/cli
 algolia import -s _site/algolia.json -a $ALGOLIA_APP_ID -k $ALGOLIA_APP_KEY -n $ALGOLIA_INDEX_NAME
-if [ $? = 0 ]; then echo "Finished Algolia index import"; else echo "Index imort fail..."; exit 1; fi
+if [ $? = 0 ]; then echo "Finished Algolia index import"; else echo "Algolia index import fail..."; exit 1; fi
 
 # build success? can deploy?
-if [ $? = 0 ]; then echo "Finished Cecil build"; exit 0; fi
+if [ $? = 0 ]; then echo "Finished build"; exit 0; fi
 
-echo "Interrupted Cecil build"; exit 1
+echo "Interrupted build"; exit 1
