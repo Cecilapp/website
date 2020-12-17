955088b7bffac4eddd14848da94356a590b27c7e
Arnaud Ligny <arnaud@ligny.org>
2020-12-17 10:58:40 +0100

    Update build.sh

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,16 +1,20 @@
 CECIL_PHAR_URL=https://github.com/Cecilapp/Cecil/releases/latest/download/cecil.phar
 CSS_INPUT=./static/tailwind.css
 CSS_OUPUT=./static/styles.css
+CACHE_PATH=/opt/build/cache/
 
 echo "Downloading Cecil"
 curl -sSOL $CECIL_PHAR_URL
 php cecil.phar --version
 
-echo "Started CSS build"
-npm install tailwindcss --silent
-npm install @tailwindcss/typography --silent
-npx tailwindcss-cli build $CSS_INPUT -o $CSS_OUPUT
-if [ $? = 0 ]; then echo "Finished CSS build"; else echo "CSS build fail..."; exit 1; fi
+if [ sha1sum -c $CACHE_PATH/hash/css.sha1 != 0 ]; then
+  echo "Started CSS build"
+  npm install tailwindcss --silent
+  npm install @tailwindcss/typography --silent
+  npx tailwindcss-cli build $CSS_INPUT -o $CSS_OUPUT
+  sha1sum $CSS_OUPUT > $CACHE_PATH/hash/css.sha1
+  if [ $? = 0 ]; then echo "Finished CSS build"; else echo "CSS build fail..."; exit 1; fi
+fi
 
 echo "Started Cecil build"
 if [[ $1 == "preview" ]]; then
