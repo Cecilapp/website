f1bf7abf4fe80759a7c73dffd9c57f18a44338d0
Arnaud Ligny <arnaud@ligny.org>
2020-12-17 15:54:30 +0100

    Update build.sh

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,12 +1,15 @@
 CECIL_PHAR_URL=https://github.com/Cecilapp/Cecil/releases/latest/download/cecil.phar
+CACHE_PATH=/opt/build/cache
 CSS_INPUT=static/tailwind.css
 CSS_OUPUT=static/styles.css
-CACHE_PATH=/opt/build/cache
+ALGOLIA_INDEX=algolia.json
+
 
 echo "Downloading Cecil"
 curl -sSOL $CECIL_PHAR_URL
 php cecil.phar --version
 
+# Build CSS
 build_css=1
 if [ -f $CACHE_PATH/$CSS_OUPUT ]; then
   cp $CACHE_PATH/$CSS_OUPUT $CSS_OUPUT
@@ -32,10 +35,21 @@ else
 fi
 if [ $? = 0 ]; then echo "Finished Cecil build"; else echo "Cecil build fail..."; exit 1; fi
 
-echo "Started Algolia index import"
-npm install -g @algolia/cli
-algolia import -s _site/algolia.json -a $ALGOLIA_APP_ID -k $ALGOLIA_APP_KEY -n $ALGOLIA_INDEX_NAME
-if [ $? = 0 ]; then echo "Finished Algolia index import"; else echo "Algolia index import fail..."; exit 1; fi
+# Import Algolia index
+import_index=1
+if [ -f $CACHE_PATH/$ALGOLIA_INDEX ]; then
+  cp $CACHE_PATH/$ALGOLIA_INDEX $ALGOLIA_INDEX
+  sha1sum -c "$CACHE_PATH/$ALGOLIA_INDEX.sha1" --status
+  import_index=$?
+fi;
+if [ $import_index = 1 ]; then
+  echo "Started Algolia index import"
+  npm install -g @algolia/cli
+  algolia import -s _site/algolia.json -a $ALGOLIA_APP_ID -k $ALGOLIA_APP_KEY -n $ALGOLIA_INDEX_NAME
+  cp $ALGOLIA_INDEX $CACHE_PATH/$ALGOLIA_INDEX
+  sha1sum $ALGOLIA_INDEX > "$CACHE_PATH/$ALGOLIA_INDEX.sha1"
+  if [ $? = 0 ]; then echo "Finished Algolia index import"; else echo "Algolia index import fail..."; exit 1; fi
+fi
 
 # build success? can deploy?
 if [ $? = 0 ]; then echo "Finished build"; exit 0; fi
