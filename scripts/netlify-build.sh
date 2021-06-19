0b870271965182bead46742f29b97c378c9e5b5b
Arnaud Ligny <arnaud@ligny.org>
2021-06-19 20:24:26 +0200

    Update netlify-build.sh

--- scripts/netlify-build.sh
+++ scripts/netlify-build.sh
@@ -13,7 +13,6 @@ if [ $? = 0 ]; then
   cp $CECIL_CACHE_DIR/$CSS_OUPUT $CSS_OUPUT
 else
   echo "Started CSS build"
-  #npm install --silent
   npx tailwindcss -i $CSS_INPUT -o $CSS_OUPUT
   if [ $? = 0 ]; then echo "Finished CSS build"; else echo "CSS build fail..."; exit 1; fi
   # cache
