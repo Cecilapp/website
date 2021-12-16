d093b547944b4e8ece5ff32e51304266830efe1e
Arnaud Ligny <arnaud@ligny.org>
2021-12-16 02:20:18 +0100

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,33 +44,33 @@ module.exports = {
         },
         dark: {
           css: {
-            color: theme('colors.gray.50'),
+            color: theme('colors.gray.300'),
             a: {
-              color: theme('colors.gray.50'),
+              color: theme('colors.gray.300'),
               '&:hover': {
-                color: theme('colors.gray.50'),
+                color: theme('colors.gray.300'),
               },
               '& code': {
-                color: theme('colors.gray.50'),
+                color: theme('colors.gray.300'),
               },
             },
             h1: {
-              color: theme('colors.gray.50'),
+              color: theme('colors.gray.300'),
             },
             h2: {
-              color: theme('colors.gray.50'),
+              color: theme('colors.gray.300'),
             },
             h3: {
-              color: theme('colors.gray.50'),
+              color: theme('colors.gray.300'),
             },
             code: {
-              color: theme('colors.gray.50'),
+              color: theme('colors.gray.300'),
             },
             blockquote: {
-              color: theme('colors.gray.50'),
+              color: theme('colors.gray.300'),
             },
             strong: {
-              color: theme('colors.gray.50'),
+              color: theme('colors.gray.300'),
             },
           },
         },
