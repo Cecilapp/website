115c4b6a093c8ba51a8495824cd5a2eee5ea996c
Arnaud Ligny <arnaud@ligny.org>
2022-03-07 02:53:26 +0100

    Add features on homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,9 @@ module.exports = {
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
       },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
