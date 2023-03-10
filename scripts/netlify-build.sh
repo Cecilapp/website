e930e2effc4ca6f054c392c1858fb17810df9b7e
Arnaud Ligny <arnaud@ligny.org>
2023-03-10 01:43:20 +0100

    Update netlify-build.sh

--- scripts/netlify-build.sh
+++ scripts/netlify-build.sh
@@ -23,7 +23,7 @@ else
   cat "$CECIL_CACHE_DIR/$CSS_INPUT.sha1"
 fi
 
-curl -H 'Accept: application/vnd.github.v3+json' 'https://api.github.com/search/repositories?q=theme+org:Cecilapp+fork:true' | jq '[.items[] | {name, full_name, description, url: .html_url, license: .license.name, homepage, date: .pushed_at, default_branch, topics}] | sort_by(.date) | reverse' > ../data/themes.json
+curl -H 'Accept: application/vnd.github.v3+json' 'https://api.github.com/search/repositories?q=theme+org:Cecilapp+fork:true' | jq '[.items[] | {name, full_name, description, url: .html_url, license: .license.name, homepage, date: .pushed_at, default_branch, topics}] | sort_by(.date) | reverse' > data/themes.json
 
 if [[ $CECIL_ENV == "production" ]]; then
   php cecil.phar build -v --baseurl=$URL --postprocess
