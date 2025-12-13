93e7392782b34d06620c25173ced1649a650fe01
Arnaud Ligny <arnaud@ligny.fr>
2025-12-13 05:05:30 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
             h1: {
               fontWeight: '700',
               paddingBottom: '.3em',
-              borderBottom: '1px solid ' + theme('colors.gray.200'),
+              borderBottom: '1px solid ' + theme('colors.slate.300'),
               marginBottom: 0,
             },
             img: {
@@ -21,6 +21,10 @@ module.exports = {
             hr: {
               marginTop: '2em',
               marginBottom: '2em',
+              color: theme('colors.slate.400'),
+            },
+            dt: {
+              color: theme('colors.darkblue'),
             },
             'a, a:hover': {
               color: theme('colors.blue'),
@@ -36,22 +40,24 @@ module.exports = {
               textDecoration: 'none',
             },
             '.anchorjs-link': {
-              color: theme('colors.gray.300'),
+              color: theme('colors.slate.400'),
             },
-
           },
         },
         dark: {
           css: {
-            color: theme('colors.gray.300'),
+            color: theme('colors.slate.300'),
             'h1, h2, h3, h4, code, blockquote, strong': {
-              color: theme('colors.gray.300'),
+              color: theme('colors.slate.300'),
+            },
+            dt: {
+              color: theme('colors.slate.300'),
             },
             'a, a:hover, a code': {
-              color: theme('colors.gray.300'),
+              color: theme('colors.slate.300'),
             },
             'thead th': {
-              color: theme('colors.gray.300'),
+              color: theme('colors.slate.300'),
             },
           },
         },
